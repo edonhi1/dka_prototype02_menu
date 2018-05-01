04029b657c2935c42dee91c893a24388f9fd510d
edonhi1 <edonhi1@gmail.com>
2018-05-01 18:10:57 +0900

    메뉴

---
+++ dka_prototype02/Assets/FoodBar.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodBar : MonoBehaviour {
+
+    public float food;
+    public float maxFood;
+    private Vector3 barScale;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        barScale = new Vector3(food / maxFood, transform.localScale.y, transform.localScale.z);
+        transform.localScale = barScale;
+	}
+}
